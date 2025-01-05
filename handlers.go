6fb0fc6599ed3fdda763ba98b1fe900893d81299
alisjj <aliyusajjads@gmail.com>
2025-01-05 11:50:51 +0100

    update: login handler to query database

--- handlers.go
+++ handlers.go
@@ -13,6 +13,12 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
 		return fmt.Errorf("error: login handler expects a username")
 	}
+	ctx := context.Background()
+
+	_, err := s.db.GetUser(ctx, cmd.args[0])
+	if err != nil {
+		return err
+	}
 
 	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
 		return err
@@ -32,16 +38,12 @@ func handlerRegister(s *state, cmd command) error {
 	created_at := time.Now()
 	updated_at := time.Now()
 	name := cmd.args[0]
-	_, err := s.db.GetUser(ctx, name)
+	c_user, err := s.db.CreateUser(ctx, database.CreateUserParams{ID: uid, CreatedAt: created_at, UpdatedAt: updated_at, Name: name})
 	if err != nil {
 		return err
 	}
-	// if user != {
-	// 	return
-	// }
-	_, err = s.db.CreateUser(ctx, database.CreateUserParams{ID: uid, CreatedAt: created_at, UpdatedAt: updated_at, Name: name})
-	if err != nil {
-		return err
-	}
+	s.cfg.CurrentUserName = c_user.Name
+	fmt.Println("User has been created!")
+	fmt.Println(c_user)
 	return nil
 }
